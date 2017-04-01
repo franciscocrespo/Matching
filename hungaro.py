b528c1a9ded2b09d3290cc7a93d70108254819f3
Francisco Crespo <francisco.crespo.jimenez@gmail.com>
2017-04-01 17:06:53 -0300

    ultimo push

--- hungaro.py
+++ hungaro.py
@@ -5,9 +5,12 @@ class Hungaro(object):
     """ En esta clase se corre el algoritmo de Matching hungaro """
 
     def __init__(self, matrix):
-        self.matrix = deepcopy(matrix)
-        self.filas = len(self.matrix)
-        self.elementos_matcheados = [[0 for i in range(self.filas)] for i in range(self.filas)]
+        self.matrix = deepcopy(matrix) # Matriz o grafo de entrada
+        self.filas = len(self.matrix) # Numero de filas, es una matriz cuadrada
+        # Matriz de macheo, tiene un 1 entre los vertices macheados
+        self.matriz_matcheo = [[0 for i in range(self.filas)] for i in range(self.filas)]
+        self.elementos_matcheados = [self.filas]
+
 
     def matching_inicial(self):
         """ Este metodo debuelve el primer matcheo de los vertices.
@@ -18,7 +21,7 @@ class Hungaro(object):
             for j in range(self.filas): #columnas
                 if self.matrix[i][j] == 0:
                     if i == 0: # para la primera fila marcamos el primer 0
-                        self.elementos_matcheados[i][j] = 1
+                        self.matriz_matcheo[i][j] = 1
                         break
                     else: # para las demas filas chequeamos si ya estan matcheadas las columnas
                         var_w = 0
@@ -26,14 +29,14 @@ class Hungaro(object):
                         # Vamos viendo las filas anteriores de la columna que contiene un 0
                         # Vemos que no este matcheada la columna que contiene el 0
                         while var_w < i: # recorremos las filas anteriores de esa columna
-                            if self.elementos_matcheados[var_w][j] == 1:
+                            if self.matriz_matcheo[var_w][j] == 1:
                                 var_count = var_count + 1 # Si está macheada lo contamos
                             var_w = var_w + 1
                         if var_count > 0: # si está matcheada no la marcamos
                             continue
                         else: # si no está matcheada la marcamos
-                            self.elementos_matcheados[i][j] = 1
-        return self.elementos_matcheados
+                            self.matriz_matcheo[i][j] = 1
+        return self.matriz_matcheo
 
 
     def resultado_matcheo(self, elementos, matriz_origuinal):
@@ -43,7 +46,7 @@ class Hungaro(object):
             var_count = 0
             var_j = 0
             for j in range(self.filas):
-                if self.elementos_matcheados[i][j] == 1:
+                if self.matriz_matcheo[i][j] == 1:
                     var_elem[i] = (elementos[i][j], matriz_origuinal[i][j])
                     var_count = 1
                     var_j = j
